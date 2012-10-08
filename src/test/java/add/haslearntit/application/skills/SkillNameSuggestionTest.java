98571bbea64c6c3c1211604ce80834d14e922e95
Krzysztof Jelski <kjelski@gmail.com>
2012-10-08 15:48:07 +0200

    Wicket tests for autocomplete behaviour.

--- src/test/java/add/haslearntit/application/skills/SkillNameSuggestionTest.java
+++ src/test/java/add/haslearntit/application/skills/SkillNameSuggestionTest.java
@@ -1,6 +1,10 @@
 package add.haslearntit.application.skills;
 
+import java.util.Arrays;
+
 import add.haslearntit.HasLearntItBaseWicketIT;
+import add.haslearntit.domain.skills.Skill;
+
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.form.AjaxFormChoiceComponentUpdatingBehavior;
 import org.apache.wicket.behavior.AbstractAjaxBehavior;
@@ -8,54 +12,52 @@ import org.apache.wicket.extensions.ajax.markup.html.autocomplete.AbstractAutoCo
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.util.tester.FormTester;
 import org.apache.wicket.util.tester.WicketTesterHelper;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SkillNameSuggestionTest extends HasLearntItBaseWicketIT {
 
+	@Before
+	public void skillsInRepository() {
+		when(skillsRepository.loadByNamePrefix("ja")).thenReturn(Arrays.asList(new Skill("Jacoco", null, null)));
+		tester.startPage(UserSkillsPage.class);
+	}
+	
 	@Test
-	@Ignore //doesn't work - http://apache-wicket.1842946.n4.nabble.com/Problem-with-testing-text-field-auto-completion-with-WicketTester-td4652722.html
 	public void shouldShowSuggestionPopup() throws InterruptedException {
 	    //given
-		tester.startPage(UserSkillsPage.class);
+		userEntersSkillNamPart("ja");
 		//when
-
-//		TextField textField = (TextField) tester.getComponentFromLastRenderedPage("newSkillForm:newSkillForm:name");
-//		System.out.println(textField.getValue());
-//		textField.getModel().setObject("ja");
-//		tester.executeAjaxEvent(textField, "onchange");
-
-		FormTester formTester = tester.newFormTester("newSkillForm:newSkillForm");
-		formTester.setValue("name", "ja");
-//		tester.executeAjaxEvent("newSkillForm:newSkillForm:name", "onkeyup");
-//		System.out.println("value: " + formTester.getTextComponentValue("name"));
-
-		AbstractAutoCompleteBehavior behavior = (AbstractAutoCompleteBehavior) WicketTesterHelper.
-				findBehavior(tester.getComponentFromLastRenderedPage("newSkillForm:newSkillForm:name"),
-						AbstractAutoCompleteBehavior.class);
-		tester.executeBehavior(behavior);
+		autocompleteIsTriggeredOn("newSkillForm:newSkillForm:name");
 		//then
 		tester.assertContains("Jacoco");
 	}
 
+	private void userEntersSkillNamPart(String skillNamePart) {
+		tester.getRequest().setParameter("q", skillNamePart);
+	}
+
 	@Test
-	@Ignore //it passes, but doesn't work properly
 	public void shouldNotShowPopupWhenNoSuggestionAvailable() {
 		//given
-		tester.startPage(UserSkillsPage.class);
+		userEntersSkillNamPart("cz");
 		//when
-		FormTester formTester = tester.newFormTester("newSkillForm:newSkillForm");
-		formTester.setValue("name", "cz");
-		AbstractAutoCompleteBehavior behavior = (AbstractAutoCompleteBehavior) WicketTesterHelper.
-				findBehavior(tester.getComponentFromLastRenderedPage("newSkillForm:newSkillForm:name"),
-						AbstractAutoCompleteBehavior.class);
-		tester.executeBehavior(behavior);
+		autocompleteIsTriggeredOn("newSkillForm:newSkillForm:name");
 		//then
 		tester.assertContainsNot("Jacoco");
+	}
+
+	private void autocompleteIsTriggeredOn(String inputPath) {
+		AbstractAutoCompleteBehavior behavior = (AbstractAutoCompleteBehavior) WicketTesterHelper.
+				findBehavior(tester.getComponentFromLastRenderedPage(inputPath),
+						AbstractAutoCompleteBehavior.class);
+		tester.executeBehavior(behavior);
 	}
 }
